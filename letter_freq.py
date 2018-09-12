7f8df7c911bb6d032bb597ff1ecd3ca5cbd8b37f
jks7743 <31747619+jks7743@users.noreply.github.com>
2018-09-12 18:53:07 -0400

    init

---
+++ letter_freq.py
@@ -0,0 +1,21 @@
+"""
+Simple letter frequency script used for analyzing shift ciphers
+"""
+
+__author__ = "Joshua Schenk"
+
+import string
+
+LETTER_DICT = dict.fromkeys(string.ascii_lowercase, 0)
+LETTER_COUNT = 0
+
+with open("input.txt", 'r') as file:
+    for line in file:
+        for ch in line:
+            for letter in LETTER_DICT:
+                if letter == ch:
+                    LETTER_DICT[letter] += 1
+                    LETTER_COUNT += 1
+
+for letter in LETTER_DICT:
+    print(letter + " " + str((LETTER_DICT[letter] / LETTER_COUNT) * 100))
